46db1df70bc8de54aa90cf507e29225cce97e773
Marshall <mn.acunab@gmail.com>
2022-07-13 17:01:35 -0300

    protected routes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+// auth middlewares
 require('./middlewares/google-auth');
 require('./middlewares/verify-token');
 
+// init conf express
 const app = express();
-
 app.use(passport.initialize());
 app.use(express.json());
 app.use(cors());
+
+// basic routes
+app.use('/', require('./routes/routes'));
+
+// protected routes
+app.use('/api', passport.authenticate('jwt', { session: false }), require('./routes/protected.routes'));
 
 port = process.env.PORT || 4000;
 
---
+++ src/routes/protected.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/users', (req, res) => {
+
+    if(req.user) {
+        return res.status(200).json({
+            ok: true,
+            msg: 'the user exist',
+            user: req.user
+        })
+    }
+
+    return res.status(400).json({
+        ok: false,
+        msg: 'the user dont exist',
+        user: null
+    })
+});
