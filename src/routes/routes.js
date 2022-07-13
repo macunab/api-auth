62b44a2cff5c2e496645673d60699de3690ebadb
Marshall <mn.acunab@gmail.com>
2022-07-13 16:53:05 -0300

    basic routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,32 @@
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const router = Router();
 
-router.get('/auth/google/')
+// Login to google account
+router.get('/auth/google', 
+    passport.authenticate('sign-in-google', {scope: ['profile', 'email']}));
+
+// Login successfull
+router.get('/aut/google/callback',
+    passport.authenticate('sign-in-google', { session: false }), 
+    (req, res) => {
+        if(req.user) {
+            const token = jwt.sign({user: req.user},
+                process.env.SECRET_KEY,
+                { expiresIn: '1h' },
+                (err, token) => {
+                    if(err) {
+                        return res.status(400).json({
+                            ok: false,
+                            msg: err.message
+                        });
+                    }
+
+                    return res.status(200).json({
+                        token
+                    });
+                });
+        }
+    });    
